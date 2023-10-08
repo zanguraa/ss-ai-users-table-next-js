76b6b2ff734680607c812ef419fc4448ebdbaae9
Valeri Zanguri <zanguraa@gmail.com>
2023-10-08 13:54:23 +0400

    feat: added edit and delete method

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUsers } from "../slices/userSlice";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AppDispatch, RootState } from "../store/store";
 import { User } from "lucide-react";
 import UserTable from "@/components/UserTable";
@@ -10,6 +10,59 @@ export default function Home() {
   const ref = useRef(false);
   const { entities, loading } = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch<AppDispatch>();
+  const [users, setUsers] = useState([]);
+
+  const onEdit = async (id, name, email) => {
+    await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          return;
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        // setUsers((users) => [...users, data]);
+        const updatedUsers = users.map((user) => {
+          if (user.id === id) {
+            user.name = name;
+            user.email = email;
+          }
+
+          return user;
+        });
+
+        setUsers((users) => updatedUsers);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onDelete = async (id) => {
+    await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          return;
+        } else {
+          setUsers(
+            users.filter((user) => {
+              return user.id !== id;
+            })
+          );
+        }
+      })
+      .catch((error) => console.log(error));
+  };
 
   console.log(entities);
 
@@ -26,7 +79,15 @@ export default function Home() {
         <div>Loading...</div>
       ) : (
         <div>
-          <UserTable store={entities} />
+          <UserTable
+            store={entities}
+            id={user.id}
+            key={user.id}
+            name={user.name}
+            email={user.email}
+            onEdit={onEdit}
+            onDelete={onDelete}
+          />
         </div>
       )}
     </div>
